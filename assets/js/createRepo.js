0c3be73461cd21d9b4b1494c902db3eddd9aafa5
Guilherme Vieira <guilhermearv3@gmail.com>
2024-06-13 17:36:44 -0300

    Testando vercel

--- assets/js/createRepo.js
+++ assets/js/createRepo.js
@@ -1,31 +1,58 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    const gitHubAPI = `https://api.github.com/users/V131R4/repos`
-    const row = document.querySelector(".repositories-row")
+const gitHubAPI = `https://api.github.com/users/V131R4/repos`;
+const gitHubProfile = `https://api.github.com/users/V131R4`;
 
-    async function getRepo(){
-        fetch(gitHubAPI)
-        .then(resp => resp.json())
-        .then(data =>{
-            data.map(item =>{
-                let div = document.createElement("div")
-                div.classList.add("col-lg-4")
-                div.innerHTML = `
-                  <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">${item.name.toUpperCase()}</h5>
-                      <p class="card-text">${item.description}</p>
-                      <p class="card-text">${Intl.DateTimeFormat('pt-BR').format(new Date(item.created_at))}</p>
-                      <a href="repo.html" class="btn btn-primary">Acesse</a>
-                    </div>
-                  </div>`
-              row.appendChild(div)
-            })
-        })
-        .catch(e =>{
-            console.log(e)
-        })
+async function getRepo(){
+  const row = document.querySelector(".repositories-row");
+      fetch(gitHubAPI)
+      .then(resp => resp.json())
+      .then(data =>{
+          data.map(item =>{
+              let div = document.createElement("div")
+              div.classList.add("col-lg-4")
+              div.innerHTML = `
+                <div class="card" style="width: 18rem;">
+                  <img src="..." class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">${item.name.toUpperCase()}</h5>
+                    <p class="card-text">${item.description}</p>
+                    <p class="card-text">${Intl.DateTimeFormat('pt-BR').format(new Date(item.created_at))}</p>
+                    <a href="repo.html" class="btn btn-primary">Acesse</a>
+                  </div>
+                </div>`
+            row.appendChild(div)
+          })
+      })
+      .catch(e =>{
+          console.log("Erro ao chamar a função", e);
+      })
+}
+
+async function getInfo(){
+  const profilePhoto = document.querySelector("#profile-photo")
+  const profileName = document.querySelector("#profile-name")
+  const profileBio = document.querySelector("#profile-bio");
+  const profileLocation = document.querySelector("#profile-location")
+  const profileUrl = document.querySelector("#profile-url")
+  const repositoriesCount = document.querySelector("#repositories-count")
+  try{
+    const response = await fetch(gitHubProfile)
+    if(!response.ok){
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
+    const data = await response.json();
+    profilePhoto.src = data.avatar_url;
+    profileName.textContent = data.name
+    profileBio.textContent = data.bio
+    profileLocation.textContent = data.location
+    profileUrl.href = data.html_url
+    profileUrl.target = `_blank`
+    repositoriesCount.textContent = `Repositórios (${data.public_repos})`
+  }catch(error){
+    console.error("Erro ao puxar o perfil!", error);
+  }
+}
 
+document.addEventListener("DOMContentLoaded", ()=>{
     getRepo()
+    getInfo()
 })
